2470fa68800ccf30049e3fc63dca11444586af8f
Hemant Khanduja <hemant.khanduja@education.gov.uk>
2026-03-13 15:57:05 +0000

    272> 279

--- src/infrastructure/terraform/locals.tf
+++ src/infrastructure/terraform/locals.tf
@@ -4,7 +4,7 @@ locals {
     "Product"          = "Support for care leavers"
     "Service Offering" = "Support for care leavers"
   }
-  prefix         = "s272${var.environment_prefix}"
-  service_prefix = "s272${var.environment_prefix}-uks-cl"
+  prefix         = "s279${var.environment_prefix}"
+  service_prefix = "s279${var.environment_prefix}-uks-cl"
   location       = "uksouth"
 }
