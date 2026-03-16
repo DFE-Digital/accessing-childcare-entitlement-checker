930efcabe05b087ed25d7b734410812287e58856
Hemant Khanduja <hemant.khanduja@education.gov.uk>
2026-03-16 18:00:35 +0000

    creating relatively cleaner branch and including pipeline

---
+++ src/infrastructure/terraform/variables.tf
@@ -0,0 +1,153 @@
+variable "elz_environment" {
+  description = "The ELZ environment to match subscription (e.g. Dev)"
+  type        = string
+}
+
+variable "environment_prefix" {
+  description = "Environment prefix (e.g. d01)"
+  type        = string
+}
+/* 
+variable "contentful_delivery_api_key" {
+  description = "Contentful Delivery API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "contentful_preview_api_key" {
+  description = "Contentful Preview API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "contentful_management_api_key" {
+  description = "Contentful Management API Key"
+  type        = string
+  sensitive   = true
+}
+
+
+variable "contentful_space_id" {
+  description = "Contentful Space ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "contentful_environment" {
+  description = "Contentful Environment"
+  type        = string
+}
+
+variable "contentful_use_preview_api" {
+  description = "Use Contentful Preview API?"
+  type        = bool
+}
+
+variable "pdf_generation_use_sandbox" {
+  description = "Generate PDFs in Sandbox Mode?"
+  type        = bool
+}
+
+variable "pdf_generation_api_key" {
+  description = "PDF Generation API Key"
+  type        = string
+  sensitive   = true
+}
+ */
+variable "caching_type" {
+  description = "Caching type"
+  type        = string
+}
+
+variable "scripts_clarity" {
+  description = "Clarity code"
+  type        = string
+}
+
+variable "custom_domain" {
+  description = "Custom front-door domain"
+  type        = string
+  default     = "abcdefghijklmnopqrstuvwxyz0123456789.azurefd.net"
+}
+
+variable "aspnetcore_environment" {
+  description = "ASP.NET Core environment"
+  type        = string
+}
+
+variable "azure_frontdoor_scale" {
+  description = "Azure Front Door Scale"
+  type        = string
+  default     = "Standard_AzureFrontDoor"
+}
+
+variable "rebrand" {
+  description = "Force DfE Rebrand before 25th June 2025"
+  type        = bool
+  default     = false
+}
+
+variable "gtaa_base_url" {
+  description = "The base url for the 'Get-To-An-Answer' questionnaire service"
+  type        = string
+}
+
+variable "support_alert_email" {
+  description = "Where to send alert emails to"
+  type        = string
+  sensitive   = true
+}
+
+variable "alerting" {
+  description = "Alerting configuration per environment"
+  type = map(object({
+    name                 = string
+    alerts_enabled       = bool
+    email_alerts_enabled = bool
+    smart_alerts_enabled = bool
+    thresholds = object({
+      availability = number
+      cpu          = number
+      memory       = number
+      error        = number
+    })
+  }))
+  default = {
+    d01 = {
+      name                 = "Test"
+      alerts_enabled       = false
+      email_alerts_enabled = false
+      smart_alerts_enabled = false
+      thresholds = {
+        availability = 90
+        cpu          = 95
+        memory       = 95
+        error        = 5
+      }
+    }
+    t01 = {
+      name                 = "Staging"
+      alerts_enabled       = true
+      email_alerts_enabled = false
+      smart_alerts_enabled = true
+      thresholds = {
+        availability = 99.9
+        cpu          = 85
+        memory       = 85
+        error        = 1
+      }
+    }
+    p01 = {
+      name                 = "Production"
+      alerts_enabled       = true
+      email_alerts_enabled = true
+      smart_alerts_enabled = true
+      thresholds = {
+        availability = 99.9
+        cpu          = 85
+        memory       = 85
+        error        = 1
+      }
+    }
+  }
+}
---
+++ src/infrastructure/terraform/web.tf
@@ -0,0 +1,125 @@
+locals {
+  web_app_settings = {
+    "ASPNETCORE_ENVIRONMENT"                = var.aspnetcore_environment
+    # "ContentfulOptions__DeliveryApiKey"     = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.contentful-delivery-api-key.versionless_id})"
+    # "ContentfulOptions__PreviewApiKey"      = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.contentful-preview-api-key.versionless_id})"
+    # "ContentfulOptions__ManagementApiKey"   = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.contentful-management-api-key.versionless_id})"
+    # "ContentfulOptions__SpaceId"            = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.contentful-space-id.versionless_id})"
+    # "ContentfulOptions__Environment"        = var.contentful_environment
+    # "ContentfulOptions__UsePreviewApi"      = var.contentful_use_preview_api
+    # "ApplicationInsights__ConnectionString" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.application-insights-connection-string.versionless_id})"
+    # "Caching__Type"                         = var.caching_type
+    # "Caching__ConnectionString"             = lower(var.caching_type) == "redis" ? "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.redis-enterprise-connection-string[0].versionless_id})" : ""
+    # "Scripts__Clarity"                      = var.scripts_clarity
+    #"AzureTranslation__AccessKey"           = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.azure-translation-access-key.versionless_id})"
+   # "PdfGeneration__ApiKey"                 = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.pdf-generation-api-key.versionless_id})"
+    # "PdfGeneration__Sandbox"                = var.pdf_generation_use_sandbox
+   # "Rebrand"                               = var.rebrand
+   # "GetToAnAnswer__BaseUrl"                = var.gtaa_base_url
+  }
+}
+
+resource "azurerm_resource_group" "web-rg" {
+  name     = "${local.prefix}rg-uks-cec-web"
+  location = local.location
+  tags     = local.common_tags
+}
+
+resource "azurerm_service_plan" "web-app-service-plan" {
+  location            = local.location
+  name                = "${local.service_prefix}-web-app-service-plan"
+  resource_group_name = azurerm_resource_group.web-rg.name
+  os_type             = "Linux"
+  sku_name            = "P0v3"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_linux_web_app_slot" "web-app-service-staging" {
+  app_service_id = azurerm_linux_web_app.web-app-service.id
+  name           = "staging"
+  https_only     = true
+
+  site_config {
+    always_on = true
+
+    ip_restriction_default_action = "Deny"
+
+    ip_restriction {
+      name        = "Access from Front Door"
+      service_tag = "AzureFrontDoor.Backend"
+    }
+
+    health_check_path                 = "/health"
+    health_check_eviction_time_in_min = 5
+
+    minimum_tls_version     = "1.3"
+    scm_minimum_tls_version = "1.3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  app_settings = local.web_app_settings
+
+  tags = local.common_tags
+}
+
+resource "azurerm_linux_web_app" "web-app-service" {
+  service_plan_id     = azurerm_service_plan.web-app-service-plan.id
+  location            = local.location
+  name                = "${local.service_prefix}-web-app-service"
+  resource_group_name = azurerm_resource_group.web-rg.name
+  https_only          = true
+
+  site_config {
+    always_on = true
+
+    ip_restriction_default_action = "Deny"
+
+    ip_restriction {
+      name        = "Access from Front Door"
+      service_tag = "AzureFrontDoor.Backend"
+    }
+
+    health_check_path                 = "/health"
+    health_check_eviction_time_in_min = 5
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  app_settings = local.web_app_settings
+
+  tags = local.common_tags
+}
+
+resource "azurerm_monitor_diagnostic_setting" "webapp_logs" {
+  name                       = "${var.environment_prefix}-web-app-diagnostics"
+  target_resource_id         = azurerm_linux_web_app.web-app-service.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log-analytics-workspace.id
+
+  # Capture common runtime and diagnostic logs
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+  }
+
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+  }
+
+  enabled_log {
+    category = "AppServicePlatformLogs"
+  }
+
+  enabled_log {
+    category = "AppServiceAppLogs"
+  }
+
+  # All Metrics
+  enabled_metric {
+    category = "AllMetrics"
+  }
+}
