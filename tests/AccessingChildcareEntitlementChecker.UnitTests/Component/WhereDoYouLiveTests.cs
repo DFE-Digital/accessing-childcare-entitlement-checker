ade49142cbc9cdbf0172a13bf3500f3296cb53e1
Steve Melia <steve.j.melia@gmail.com>
2026-03-17 12:17:51 +0000

    AC-437: Add a component test using WebApplicationFactory

---
+++ tests/AccessingChildcareEntitlementChecker.UnitTests/Component/WhereDoYouLiveTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace AccessingChildcareEntitlementChecker.UnitTests.Component
+{
+    public class WhereDoYouLiveTests
+    {
+        [Fact]
+        public async Task GetRootReturnsWhereDoYouLivePage()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            var response = await client.GetAsync("/");
+            var body = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("Where do you live?", body);
+        }
+    }
+}
