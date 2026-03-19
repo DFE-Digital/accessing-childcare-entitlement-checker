011bab69f1ed429028ab02df7ca730c05cf60b90
hemant-khanduja <hemantkhanduja@gmail.com>
2026-03-19 15:48:52 +0000

    Feature/hk ac480 tf script (#41)
    * creating relatively cleaner branch and including pipeline
    
    * fixing az login error
    
    * fix attempt 2
    
    * adding step to setup terraform
    
    * removing comments
    
    * clean up
    
    * bring back elz env
    
    * making the app public to aid development and using ukwest ..will add front door and auto scale options later
    
    * bring back security
    
    * whitespace
    
    * clean up
    
    * cleaning this placeholder file for now
    
    * cleanup
    
    * cleaned few more entries

---
+++ src/infrastructure/terraform/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  common_tags = {
+    "Environment"      = var.elz_environment
+    "Product"          = "Childcare Entitlement Checker"
+    "Service Offering" = "Eligibility Service"
+  }
+  prefix         = "s279${var.environment_prefix}"
+  service_prefix = "s279${var.environment_prefix}-uks-cec"
+  location       = "ukwest"
+}
---
+++ src/infrastructure/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "elz_environment" {
+  description = "The ELZ environment to match subscription (e.g. Dev)"
+  type        = string
+}
+
+variable "environment_prefix" {
+  description = "Environment prefix (e.g. d01)"
+  type        = string
+}
+
+
+variable "aspnetcore_environment" {
+  description = "ASP.NET Core environment"
+  type        = string
+}
+
