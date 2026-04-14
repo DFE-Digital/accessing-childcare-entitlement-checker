8eac73e02173f9d706c87cd796c9d486dcd31b67
Steve Melia <steve.j.melia@gmail.com>
2026-04-14 14:36:12 +0100

    Fettling Program.cs

--- src/AccessingChildcareEntitlementChecker.Web/Program.cs
+++ src/AccessingChildcareEntitlementChecker.Web/Program.cs
@@ -6,67 +6,52 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-
 builder.Services
-    .AddControllersWithViews(options =>
-    {
-        options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-    })
+    .AddLocalization(options => options.ResourcesPath = "Resources")
+    .AddDistributedMemoryCache()
+    .AddSession()
+    .AddHttpContextAccessor()
+    .AddScoped<IJourneySession, JourneySession>()
+    .AddGovUkFrontend();
+
+builder.Services.AddControllersWithViews(options =>
+        {
+            options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+        })
     .AddViewLocalization();
 
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession();
-
-builder.Services.AddHttpContextAccessor();
-builder.Services
-    .AddScoped<IJourneySession, JourneySession>()
-    .AddHealthChecks();
-
-builder.Services.AddGovUkFrontend();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
-
-var supportedCultures = new[] { new CultureInfo("en-GB") };
-
-app.UseRequestLocalization(new RequestLocalizationOptions
-{
-    DefaultRequestCulture = new RequestCulture("en-GB"),
-    SupportedCultures = supportedCultures,
-    SupportedUICultures = supportedCultures
-});
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Entitlement/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseDevelopmentAuth(builder.Configuration);
     app.MapRobotsExclusionProtocol();
 }
+else
+{
+    app.UseExceptionHandler("/Entitlement/Error")
+        .UseHsts();
+}
 
-app.UseStaticFiles();
-
-app.UseGovUkFrontend();
-
-app.UseRouting();
-
-app.UseSession();
-
-app.UseAuthorization();
+var supportedCultures = new[] { new CultureInfo("en-GB") };
+app.UseRequestLocalization(new RequestLocalizationOptions
+    {
+        DefaultRequestCulture = new RequestCulture("en-GB"),
+        SupportedCultures = supportedCultures,
+        SupportedUICultures = supportedCultures
+    })
+    .UseHttpsRedirection()
+    .UseStaticFiles()
+    .UseGovUkFrontend()
+    .UseRouting()
+    .UseSession()
+    .UseAuthorization()
+    .UseExceptionHandler("/Error");
 
 app.MapHealthChecks("/health");
-
-app.UseExceptionHandler("/Error");
-
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Start}/{id?}");
+        name: "default",
+        pattern: "{controller=Home}/{action=Start}/{id?}");
 
 app.Run();
