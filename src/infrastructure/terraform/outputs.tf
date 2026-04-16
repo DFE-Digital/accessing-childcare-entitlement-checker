9da1eea3644232c1cfda38d6e078d7c607c1f2a6
Steve Melia <steve.melia@education.gov.uk>
2026-04-16 10:01:38 +0100

    AC-485: Add federated credential, deployment and front door. (#56)

--- src/infrastructure/terraform/outputs.tf
+++ src/infrastructure/terraform/outputs.tf
@@ -6,6 +6,6 @@ output "web_app_name" {
   value = azurerm_linux_web_app.web-app-service.name
 }
 
-output "web_app_slot_name" {
-  value = azurerm_linux_web_app_slot.web-app-service-staging.name
+output "frontdoor_hostname" {
+  value = var.custom_domain != "" ? var.custom_domain : azurerm_cdn_frontdoor_endpoint.frontdoor-web-endpoint.host_name
 }
