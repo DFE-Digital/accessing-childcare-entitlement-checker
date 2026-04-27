dfc27ff3da4e70676565b2a2bb107a3279dc9e8a
Steve Melia <steve.j.melia@gmail.com>
2026-04-27 11:08:20 +0100

    Add tests for session

--- tests/AccessingChildcareEntitlementChecker.UnitTests/Controllers/UserControllerTests.cs
+++ tests/AccessingChildcareEntitlementChecker.UnitTests/Controllers/UserControllerTests.cs
@@ -27,13 +27,6 @@ public class UserControllerTests
     }
 
     [Fact]
-    public void UserAge_ReturnsView()
-    {
-        var result = _controller.UserAge();
-        Assert.Null(result.Model<UserAgeViewModel>().UserAge);
-    }
-
-    [Fact]
     public void HasPartner_Get_PopulatesModel_FromState()
     {
         _journeyState.HasPartner = true;
@@ -41,46 +34,23 @@ public class UserControllerTests
         Assert.True(result.Model<HasPartnerViewModel>().HasPartner);
     }
 
-    [Fact]
-    public void UserAge_Get_PopulatesModel_FromState()
-    {
-        _journeyState.UserAge = AgeRange.EighteenToTwenty;
-        var result = _controller.UserAge();
-        Assert.Equal(AgeRange.EighteenToTwenty, result.Model<UserAgeViewModel>().UserAge);
-    }
-
-    [Fact]
-    public void HasPartner_Post_ValidSelection_SavesState_AndRedirects()
+    [Theory]
+    [InlineData(true, nameof(PartnerController.PartnerAge))]
+    [InlineData(false, nameof(UserController.NextStepPlaceholder))]
+    public void HasPartner_Post_ValidSelection_SavesState_AndRedirects(bool hasPartner, string redirectsTo)
     {
         var model = new HasPartnerViewModel()
         {
-            HasPartner = true
+            HasPartner = hasPartner,
         };
 
         var result = _controller.HasPartner(model);
 
         var redirect = Assert.IsType<RedirectToActionResult>(result);
         _journeySession.Received(1).Set(_journeyState);
-        Assert.Equal(true, _journeyState.HasPartner);
+        Assert.Equal(hasPartner, _journeyState.HasPartner);
         Assert.True(_controller.ModelState.IsValid);
-        Assert.Equal(nameof(PartnerController.PartnerAge), redirect.ActionName);
-    }
-
-    [Fact]
-    public void UserAge_Post_ValidSelection_SavesState_AndRedirects()
-    {
-        var model = new UserAgeViewModel()
-        {
-            UserAge = AgeRange.EighteenToTwenty
-        };
-
-        var result = _controller.UserAge(model);
-
-        var redirect = Assert.IsType<RedirectToActionResult>(result);
-        _journeySession.Received(1).Set(_journeyState);
-        Assert.Equal(AgeRange.EighteenToTwenty, _journeyState.UserAge);
-        Assert.True(_controller.ModelState.IsValid);
-        Assert.Equal(nameof(PartnerController.PartnerAge), redirect.ActionName);
+        Assert.Equal(redirectsTo, redirect.ActionName);
     }
 
     [Fact]
@@ -98,6 +68,38 @@ public class UserControllerTests
         var view = Assert.IsType<ViewResult>(result);
         Assert.False(_controller.ModelState.IsValid);
         Assert.True(_controller.ModelState.ContainsKey(nameof(model.HasPartner)));
+    }
+
+    [Fact]
+    public void UserAge_ReturnsView()
+    {
+        var result = _controller.UserAge();
+        Assert.Null(result.Model<UserAgeViewModel>().UserAge);
+    }
+
+    [Fact]
+    public void UserAge_Get_PopulatesModel_FromState()
+    {
+        _journeyState.UserAge = AgeRange.EighteenToTwenty;
+        var result = _controller.UserAge();
+        Assert.Equal(AgeRange.EighteenToTwenty, result.Model<UserAgeViewModel>().UserAge);
+    }
+
+    [Fact]
+    public void UserAge_Post_ValidSelection_SavesState_AndRedirects()
+    {
+        var model = new UserAgeViewModel()
+        {
+            UserAge = AgeRange.EighteenToTwenty
+        };
+
+        var result = _controller.UserAge(model);
+
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        _journeySession.Received(1).Set(_journeyState);
+        Assert.Equal(AgeRange.EighteenToTwenty, _journeyState.UserAge);
+        Assert.True(_controller.ModelState.IsValid);
+        Assert.Equal(nameof(PartnerController.PartnerAge), redirect.ActionName);
     }
 
     [Fact]
