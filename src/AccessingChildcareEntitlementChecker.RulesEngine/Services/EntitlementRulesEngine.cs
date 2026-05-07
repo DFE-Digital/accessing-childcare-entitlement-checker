a8817cb1094eedabd9e8409f94ba10fdca5f7ef1
Carl Louden <carllouden@Carls-MacBook-Pro.local>
2026-05-07 12:43:04 +0100

    remove test code

--- src/AccessingChildcareEntitlementChecker.RulesEngine/Services/EntitlementRulesEngine.cs
+++ src/AccessingChildcareEntitlementChecker.RulesEngine/Services/EntitlementRulesEngine.cs
@@ -2,28 +2,5 @@ namespace AccessingChildcareEntitlementChecker.RulesEngine.Services;
 
 public class EntitlementRulesEngine
 {
-    public class SonarTest
-    {
-        public void BadMethod()
-        {
-            var a = 1;
-            var b = 1;
-            var c = 1;
-            var d = 1;
-            var e = 1;
-            var f = 1;
-            var g = 1;
-        }
 
-        public void BadMethod2()
-        {
-            var a = 1;
-            var b = 1;
-            var c = 1;
-            var d = 1;
-            var e = 1;
-            var f = 1;
-            var g = 1;
-        }
-    }
 }
