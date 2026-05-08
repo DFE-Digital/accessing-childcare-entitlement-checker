724f2248894bb715414c83e7a9c43ac2cff612bb
carllouden <carl.louden@hippodigital.co.uk>
2026-05-08 15:02:42 +0100

    AC-615 Rules Engine 2 - Define request and response DTOs for rules engine (#86)
    * Inital commit for rules engine 2 ticket
    
    * Build the Response dtos
    
    * remove shared references
    
    * fix up work status to account for multioption
    
    * Formatting
    
    * Fix file path formatting in solution file
    
    * adr updates
    
    * adr refinement
    
    * Add BirthStatus to align with web layer enum
    
    ---------
    
    Co-authored-by: Carl Louden <carllouden@Carls-MacBook-Pro.local>

---
+++ src/AccessingChildcareEntitlementChecker.RulesEngine/Dtos/Responses/EntitlementReponse.cs
@@ -0,0 +1,6 @@
+namespace AccessingChildcareEntitlementChecker.RulesEngine.Dtos.Responses;
+
+public class EntitlementResponse
+{
+    public List<ChildResultDto> ChildResults { get; set; } = [];
+}
