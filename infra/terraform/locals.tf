06311b014b2c87ca304412aa6626ec7a28b9373b
Jonathan Abbott <>
2026-06-06 11:16:57 +0100

    ﻿feat: add terraform bootstrap to setup state storage

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -3,6 +3,8 @@ locals {
     "Environment"      = var.elz_environment
     "Product"          = "Improve user journeys in early education and childcare"
     "Service Offering" = "Improve user journeys in early education and childcare"
+    "Source"           = "terraform"
+
   }
   prefix         = "s279${var.environment_prefix}"
   service_prefix = "s279${var.environment_prefix}-uks-cec"
