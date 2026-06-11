933ed4823adcd0f62448ff4b271327d85ad4c00b
Jon Abbott <jonathan.abbott@education.gov.uk>
2026-06-11 09:02:02 +0100

    feat: add private link to front door for backend origin (#221)

--- infra/terraform/environments/production.tfvars
+++ infra/terraform/environments/production.tfvars
@@ -2,5 +2,7 @@ elz_environment        = "Prod"
 environment_prefix     = "p01"
 aspnetcore_environment = "Production"
 webapp_sku             = "P1v2"
+azure_frontdoor_scale  = "Premium"
+webapp_zone_balancing  = true
 webapp_instance_count  = 2
 enable_staging_slot    = true
--- infra/terraform/frontdoor_waf.tf
+++ infra/terraform/frontdoor_waf.tf
@@ -6,7 +6,7 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "web_firewall_policy" {
   sku_name            = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.sku_name
 
   dynamic "managed_rule" {
-    for_each = local.is_premium_fd ? [0] : []
+    for_each = var.azure_frontdoor_scale == "Premium" ? [0] : []
     content {
       type    = "Microsoft_DefaultRuleSet"
       version = "2.1"
@@ -15,7 +15,7 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "web_firewall_policy" {
   }
 
   dynamic "managed_rule" {
-    for_each = local.is_premium_fd ? [0] : []
+    for_each = var.azure_frontdoor_scale == "Premium" ? [0] : []
     content {
       type    = "Microsoft_BotManagerRuleSet"
       version = "1.1"
