59ef75969a62e75071cdab6c5a509f86fcb67180
Jon Abbott <jonathan.abbott@education.gov.uk>
2026-06-17 11:15:01 +0100

    feat: enhance deployment storage settings (#249)

--- infra/terraform/storage.tf
+++ infra/terraform/storage.tf
@@ -1,18 +1,21 @@
 resource "azurerm_storage_account" "deployment_storage" {
   #checkov:skip=CKV2_AZURE_1:Microsoft-managed encryption is sufficient for this storage account
   #checkov:skip=CKV_AZURE_33:Azure Queue Storage is not used by this storage account
+  #checkov:skip=CKV_AZURE_206: ZRS is intentionally used to provide zonal resilience aligned with the App Service architecture.
   name                            = "${local.prefix}webappdeploy"
   resource_group_name             = azurerm_resource_group.web-rg.name
   location                        = local.location
   account_tier                    = "Standard"
-  account_replication_type        = "RAGRS"
+  account_replication_type        = "ZRS"
   public_network_access_enabled   = false
   shared_access_key_enabled       = false
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
+  access_tier                     = "Smart"
   tags                            = local.common_tags
 
   blob_properties {
+    last_access_time_enabled = true
     delete_retention_policy {
       days = 30
     }
@@ -97,4 +100,29 @@ resource "azurerm_private_endpoint" "deployment_pe" {
   depends_on = [
     azurerm_private_dns_zone_virtual_network_link.blob_dns_link
   ]
+}
+
+resource "azurerm_storage_management_policy" "deployment_policy" {
+  storage_account_id = azurerm_storage_account.deployment_storage.id
+
+  rule {
+    name    = "cleanup-old-non-retained-packages"
+    enabled = true
+
+    filters {
+      blob_types = ["blockBlob"]
+
+      match_blob_index_tag {
+        name      = "retain"
+        operation = "=="
+        value     = "false"
+      }
+    }
+
+    actions {
+      base_blob {
+        delete_after_days_since_last_access_time_greater_than = 180
+      }
+    }
+  }
 }
