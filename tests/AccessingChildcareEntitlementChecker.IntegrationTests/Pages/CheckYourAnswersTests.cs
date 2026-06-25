684e69cf0091a2b7166a4e95048754130273dbfe
Steve Melia <steve.melia@education.gov.uk>
2026-06-25 09:59:53 +0100

    feat: add backlink tests for check your answers page (#284)
    refs: AC-810

---
+++ tests/AccessingChildcareEntitlementChecker.IntegrationTests/Pages/CheckYourAnswersTests.cs
@@ -0,0 +1,31 @@
+using AccessingChildcareEntitlementChecker.IntegrationTests.Fixtures;
+using AccessingChildcareEntitlementChecker.IntegrationTests.Helpers;
+using AccessingChildcareEntitlementChecker.Web.Models.Partner;
+using AccessingChildcareEntitlementChecker.Web.Services;
+
+namespace AccessingChildcareEntitlementChecker.IntegrationTests.Pages;
+
+public class CheckYourAnswersTests(IntegrationTestFixture factory) : IClassFixture<IntegrationTestFixture>
+{
+    [Theory]
+    [InlineData(false, null, "/partner")]
+    [InlineData(true, null, "/benefits/childcare-support-partner")]
+    [InlineData(true, PartnerChildcareSupportOption.ChildcareVouchers, "/benefits/childcare-vouchers-partner")]
+    public async Task Get_Has_BackLink(
+        bool? hasPartner,
+        PartnerChildcareSupportOption? partnerChildcareSupport,
+        string backLinkUrl)
+    {
+        using var client = factory.CreateClientWithJourneyState(new JourneyState
+        {
+            HasPartner = hasPartner,
+            PartnerChildcareSupport = partnerChildcareSupport.HasValue ? [partnerChildcareSupport.Value] : [],
+        });
+
+        var url = $"/check-your-answers";
+        var response = await client.GetAsync(url, TestContext.Current.CancellationToken);
+        response.EnsureSuccessStatusCode();
+        var doc = await HtmlHelpers.ParseHtmlAsync(response.Content);
+        doc.AssertBackLink(backLinkUrl);
+    }
+}
