3cbb91ef4d519b20884e196ef3cea6fd9fddbe57
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2026-07-24 10:13:04 +0100

    fix(sonar): make CorrelationId nullable in submit models to avoid under-posting

--- src/AccessingChildcareEntitlementChecker.Web/Models/Summary/CheckAnswersSubmitModel.cs
+++ src/AccessingChildcareEntitlementChecker.Web/Models/Summary/CheckAnswersSubmitModel.cs
@@ -1,3 +1,3 @@
 namespace AccessingChildcareEntitlementChecker.Web.Models.Summary;
 
-public record CheckAnswersSubmitModel(Guid CorrelationId);
+public record CheckAnswersSubmitModel(Guid? CorrelationId);
