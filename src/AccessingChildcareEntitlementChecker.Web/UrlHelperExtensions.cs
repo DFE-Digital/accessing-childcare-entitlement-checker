91d863ed6d28a088d766585943f0dfc1fd3e2106
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2026-07-30 16:54:43 +0100

    refactor(web): consolidate back link fallback logic into UrlHelper extension

--- src/AccessingChildcareEntitlementChecker.Web/UrlHelperExtensions.cs
+++ src/AccessingChildcareEntitlementChecker.Web/UrlHelperExtensions.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AccessingChildcareEntitlementChecker.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AccessingChildcareEntitlementChecker.Web;
 
 public static class UrlHelperExtensions
 {
+    public static string GetBackLinkOrAction(this IUrlHelper urlHelper, string? returnTo, string actionName, string? controllerName = null, object? routeValues = null)
+    {
+        if (ReturnTo.TryGetReturnToUrl(urlHelper, returnTo, out var url))
+        {
+            return url;
+        }
+
+        return controllerName == null
+            ? urlHelper.ActionOrThrow(actionName, routeValues)
+            : urlHelper.ActionOrThrow(actionName, controllerName, routeValues);
+    }
+
     public static string ActionOrThrow(this IUrlHelper urlHelper, string actionName, object? routeValues = null)
     {
         var url = urlHelper.Action(actionName, routeValues);
