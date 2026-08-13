5fe8ca03d7eb4a01858397fb64f87f5924df9929
Carl Louden <carllouden@Carls-MacBook-Pro.local>
2026-08-13 13:20:45 +0100

    Create partial for notifcation

---
+++ src/AccessingChildcareEntitlementChecker.Web/Views/Summary/_RemovedChildrenNotification.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AccessingChildcareEntitlementChecker.Web.Views.Summary;
+
+public class _RemovedChildrenNotification : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
