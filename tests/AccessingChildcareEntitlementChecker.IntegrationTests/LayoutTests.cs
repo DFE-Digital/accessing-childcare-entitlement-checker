2435c8257b929a1fb41af69841dbe4fd751c1601
Steve Melia <steve.j.melia@gmail.com>
2026-08-14 12:33:41 +0100

    add test

--- tests/AccessingChildcareEntitlementChecker.IntegrationTests/LayoutTests.cs
+++ tests/AccessingChildcareEntitlementChecker.IntegrationTests/LayoutTests.cs
@@ -1,5 +1,7 @@
 using AccessingChildcareEntitlementChecker.IntegrationTests.Fixtures;
 using AccessingChildcareEntitlementChecker.IntegrationTests.Helpers;
+using AccessingChildcareEntitlementChecker.Web.Models;
+using AccessingChildcareEntitlementChecker.Web.Services;
 using AngleSharp.Html.Dom;
 
 namespace AccessingChildcareEntitlementChecker.IntegrationTests;
@@ -23,17 +25,36 @@ public class LayoutTests(IntegrationTestFixture factory) : IClassFixture<Integra
     }
 
     [Fact]
-    public async Task LayoutFeedbackLinkOpensInNewTab()
+    public async Task LayoutIncludesRequiredFooterLinks()
     {
-        using var client = factory.CreateClient();
-        var response = await client.GetAsync("/cookies", TestContext.Current.CancellationToken);
-        response.EnsureSuccessStatusCode();
+        var routesWithoutFooter = new[]
+        {
+            "/throw",
+            "/robots.txt",
+        };
 
-        var document = await HtmlHelpers.ParseHtmlAsync(response.Content);
+        var routes = RouteHelper.GetEndpointsExcept(factory, "GET", routesWithoutFooter);
+        foreach (var url in routes)
+        {
+            using var getClient = factory.CreateClientWithJourneyState(new JourneyState
+            {
+                Children = new Dictionary<string, Child>
+                {
+                   {
+                       "1",
+                       new Child("1", "Child 1")
+                   }
+                },
+            });
 
-        var feedbackLink = document.QuerySelector("a[href*='dferesearch.fra1.qualtrics.com']");
-        Assert.NotNull(feedbackLink);
-        Assert.Equal("_blank", feedbackLink.GetAttribute("target"));
-        Assert.Equal("noopener noreferrer", feedbackLink.GetAttribute("rel"));
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var getResponse = await getClient.SendAsync(request, TestContext.Current.CancellationToken);
+            var document = await HtmlHelpers.ParseHtmlAsync(getResponse.Content);
+            document
+                .AssertFooterContainsLink("https://accessibility-statements.education.gov.uk/s/89") // accessibility statement
+                .AssertFooterContainsLink("https://www.gov.uk/government/publications/privacy-information-members-of-the-public/privacy-information-members-of-the-public#using-your-data-when-you-use-our-websites") // privacy notice
+                .AssertFooterContainsLink("/cookies") // cookies
+                .AssertFooterContainsLink("https://dferesearch.fra1.qualtrics.com/jfe/form/SV_5doFVpOqJt1dD7g"); // contact us & feedback form
+        }
     }
 }
