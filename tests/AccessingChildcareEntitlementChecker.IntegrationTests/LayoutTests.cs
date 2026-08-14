286740a3e6e2578b5698322f6c56a2a687671a51
Steve Melia <steve.j.melia@gmail.com>
2026-08-14 16:05:09 +0100

    Review feedback: html and remove overly-clever test

--- tests/AccessingChildcareEntitlementChecker.IntegrationTests/LayoutTests.cs
+++ tests/AccessingChildcareEntitlementChecker.IntegrationTests/LayoutTests.cs
@@ -1,8 +1,5 @@
 using AccessingChildcareEntitlementChecker.IntegrationTests.Fixtures;
 using AccessingChildcareEntitlementChecker.IntegrationTests.Helpers;
-using AccessingChildcareEntitlementChecker.Web.Models;
-using AccessingChildcareEntitlementChecker.Web.Services;
-using AngleSharp.Html.Dom;
 
 namespace AccessingChildcareEntitlementChecker.IntegrationTests;
 
@@ -27,34 +24,14 @@ public class LayoutTests(IntegrationTestFixture factory) : IClassFixture<Integra
     [Fact]
     public async Task LayoutIncludesRequiredFooterLinks()
     {
-        var routesWithoutFooter = new[]
-        {
-            "/throw",
-            "/robots.txt",
-        };
-
-        var routes = RouteHelper.GetEndpointsExcept(factory, "GET", routesWithoutFooter);
-        foreach (var url in routes)
-        {
-            using var getClient = factory.CreateClientWithJourneyState(new JourneyState
-            {
-                Children = new Dictionary<string, Child>
-                {
-                   {
-                       "1",
-                       new Child("1", "Child 1")
-                   }
-                },
-            });
-
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
-            var getResponse = await getClient.SendAsync(request, TestContext.Current.CancellationToken);
-            var document = await HtmlHelpers.ParseHtmlAsync(getResponse.Content);
-            document
-                .AssertFooterContainsLink("https://accessibility-statements.education.gov.uk/s/89") // accessibility statement
-                .AssertFooterContainsLink("https://www.gov.uk/government/publications/privacy-information-members-of-the-public/privacy-information-members-of-the-public#using-your-data-when-you-use-our-websites") // privacy notice
-                .AssertFooterContainsLink("/cookies") // cookies
-                .AssertFooterContainsLink("https://dferesearch.fra1.qualtrics.com/jfe/form/SV_5doFVpOqJt1dD7g"); // contact us & feedback form
-        }
+        using var getClient = factory.CreateClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, "/");
+        var getResponse = await getClient.SendAsync(request, TestContext.Current.CancellationToken);
+        var document = await HtmlHelpers.ParseHtmlAsync(getResponse.Content);
+        document
+            .AssertFooterContainsLink("https://accessibility-statements.education.gov.uk/s/89") // accessibility statement
+            .AssertFooterContainsLink("https://www.gov.uk/government/publications/privacy-information-members-of-the-public/privacy-information-members-of-the-public#using-your-data-when-you-use-our-websites") // privacy notice
+            .AssertFooterContainsLink("/cookies") // cookies
+            .AssertFooterContainsLink("https://dferesearch.fra1.qualtrics.com/jfe/form/SV_5doFVpOqJt1dD7g"); // contact us & feedback form
     }
 }
