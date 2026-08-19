421cea209187596d24466ab6db8aa9afb6ab5b18
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2026-08-19 12:36:03 +0100

    fix(shutter): place checkov skip comments inside resource blocks for correct parsing

--- infra/terraform/frontdoor.tf
+++ infra/terraform/frontdoor.tf
@@ -78,8 +78,8 @@ resource "azurerm_cdn_frontdoor_origin_group" "shutter-origin-group" {
   }
 }
 
-# checkov:skip=CKV_SECRET_6: User Assigned Identity ID is a standard resource ID, not a secret.
 resource "azapi_update_resource" "shutter_origin_group_auth" {
+  # checkov:skip=CKV_SECRET_6: User Assigned Identity ID is a standard resource ID, not a secret.
   type        = "Microsoft.Cdn/profiles/originGroups@2023-05-01"
   resource_id = azurerm_cdn_frontdoor_origin_group.shutter-origin-group.id
 
