25d407f257d080ad602dd4e951e778e5ce304678
carllouden <carl.louden@hippodigital.co.uk>
2026-09-09 11:48:48 +0100

    Hotfix: validation on 100k and UC question (#559)
    Co-authored-by: Carl Louden <carllouden@Carls-MacBook-Pro.local>

--- src/AccessingChildcareEntitlementChecker.Web/Validators/JourneyStateValidator.cs
+++ src/AccessingChildcareEntitlementChecker.Web/Validators/JourneyStateValidator.cs
@@ -134,9 +134,13 @@ public class JourneyStateValidator : AbstractValidator<JourneyState>
 
                 });
 
-
-            RuleFor(x => x.UniversalCredit)
-                .NotNull();
+            When(x =>
+                    x.YearlyEarnings != YearlyEarningsOption.AboveThreshold,
+                () =>
+                {
+                    RuleFor(x => x.UniversalCredit)
+                        .NotNull();
+                });
 
             RuleFor(x => x.Benefits)
                 .NotEmpty();
