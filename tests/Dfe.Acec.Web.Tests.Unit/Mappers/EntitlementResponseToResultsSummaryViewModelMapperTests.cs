353e9bf6b4f6f14aa3103f34e4eb0baa68f6fcd2
Carl Louden <carllouden@Carls-MacBook-Pro.local>
2026-09-23 17:43:51 +0100

    Add test for mapping scheme url

--- tests/Dfe.Acec.Web.Tests.Unit/Mappers/EntitlementResponseToResultsSummaryViewModelMapperTests.cs
+++ tests/Dfe.Acec.Web.Tests.Unit/Mappers/EntitlementResponseToResultsSummaryViewModelMapperTests.cs
@@ -573,6 +573,20 @@ public class EntitlementResponseToResultsSummaryViewModelMapperTests
         Assert.Equal(expectedText, scheme.WhenToApply);
     }
 
+    [Fact]
+    public void MapSchemeUrl()
+    {
+        var response = CreateTestEntitlementResponse();
+
+        var result = _mapper.Map(response);
+
+        var child = result.Children.Single(x => x.Name == "Katie");
+
+        var scheme = child.Schemes.Single(x => x.SchemeCode == SchemeCode.TaxFreeChildcare);
+
+        Assert.Equal("https://www.gov.uk/tax-free-childcare", scheme.Url);
+    }
+
 }
 
 
